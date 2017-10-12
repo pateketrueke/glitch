0a23e7cb4f6d74ffd53ceda2a0afd489c9f689d7
System <none>
2017-10-12 08:29:55 +0000

    Import of pateketrueke/glitch

--- src/assets/javascripts/app.js
+++ src/assets/javascripts/app.js
@@ -1,16 +1,16 @@
 import App from './App.vue.pug';
 
-/* global Vue */
-
-const vm = new Vue({
-  el: document.getElementById('app'),
-  render: h => h(App),
-});
-
 /* global version, environment */
 
 const message = `Grown v${version} / ENV: ${environment}`;
 
-console.log(message);
+/* global Vue */
 
-vm.value = message;
+const vm = new Vue({
+  el: document.getElementById('app'),
+  render: h => h(App, {
+    props: {
+      value: message,
+    },
+  }),
+});
